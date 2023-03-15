8fd8e5af5c918fdfe4e65729a45d8a164d57d9eb
Kathllyn Santos <120657741+KathllynLeticia@users.noreply.github.com>
2023-03-15 15:24:04 -0300

    Update UsuarioService.java

--- src/main/java/org/generation/vagalume/service/UsuarioService.java
+++ src/main/java/org/generation/vagalume/service/UsuarioService.java
@@ -93,7 +93,7 @@ public class UsuarioService {
 
 	        String token = usuario + ":" + senha;
 	        byte[] tokenBase64 = Base64.encodeBase64(token.getBytes(Charset.forName("US-ASCII")));
-	        return "Basic" + new String(tokenBase64);
+	        return "Basic " + new String(tokenBase64);
 
 	    }
 }
